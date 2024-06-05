8447e4bb37201bc95288ae3962c0fba38cb8d5f7
Aron <aron.cardoso1@gmail.com>
2024-06-05 11:23:10 -0300

    First Commit

---
+++ apiConsumption/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiConsumption.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ apiConsumption/Program.cs
@@ -0,0 +1,28 @@
+﻿using apiConsumption.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apiConsumption
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+
+            string responseBody = await client.GetStringAsync("/users");
+            var users = JsonConvert.DeserializeObject<List<User>>(responseBody);
+
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Name: {user.Name}\nEmail: {user.Email}\nUsername: {user.Username}\n" +
+                    $"City: {user.Address.City}, {user.Address.Street}\n");
+            }
+        }
+    }
+}
